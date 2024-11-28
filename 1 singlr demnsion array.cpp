8205aa446ba981bf6da24bc63e14f7bd3d85ac59
faisal22332 <faisaltehseen868@gmail.com>
2024-11-27 21:23:01 -0800

    Add files via upload
    I have added single array file

---
+++ 1 singlr demnsion array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int marks[5];
+	int i;
+	cout<<"enter your marks:";
+	for(i=0;i<5;i++)
+	{
+		cin>>marks[i];
+	}
+	cout<<"your marks:"<<endl;
+	for(i=0;i<5;i++){
+	
+	cout<<marks[i]<<endl;
+}
+return 0;
+}
